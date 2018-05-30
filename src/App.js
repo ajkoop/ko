79de8427dbaf2b7bc737e418c697d011e285c578
Andrew Koop <koop@indiana.edu>
2018-05-30 01:06:49 -0400

    shup

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           Welcome to my tiny site.</p> <p>I'm a database programmer and educator.</p> <p> I live in a green little place called Bloomington, IN.</p>
-        <h2>Who Me?</h2>
-        <p>I studied to be a copywriter and then wrote grants.</p>
-        <p> Then I made a great many websites. These days, I mostly write data integration Powershell scripts for Microsoft, Google, Apple Classroom, and any other apps the kids need their paws on.</p>
+        <h2>My Work</h2>
+        <p>I studied to be a copywriter and wrote grants. Then, I made a great many websites.</p>
+        <p>These days, I write data integration Powershell scripts for Microsoft, Google, Apple Classroom, and any other apps the kids need their paws on.</p>
       </div>
     );
   }
