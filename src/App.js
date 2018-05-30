9bc50a01e029f998fd994ec05f4d9325de57fea3
Andrew Koop <koop@indiana.edu>
2018-05-30 06:50:40 -0400

    header change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           Hi, there.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
-        <h2>My Work</h2>
+        <h2>WORKworkWORK</h2>
         <p>I studied to be a copywriter and wrote grants. Then, I made websites.</p>
         <p>Today, I write data integration Powershell scripts for Microsoft, Google, Apple Classroom, and any other apps the kids need their paws on.</p>
       </div>
