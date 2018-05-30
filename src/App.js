f8d236fcb788e490e69fdd6ebf79232d2bc54f4b
Andrew Koop <koop@indiana.edu>
2018-05-30 06:48:34 -0400

    pango

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           Hi, there.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
         <h2>My Work</h2>
-        <p>I studied to be a copywriter and wrote grants. Then, I made a great many websites.</p>
+        <p>I studied to be a copywriter and wrote grants. Then, I made websites.</p>
         <p>Today, I write data integration Powershell scripts for Microsoft, Google, Apple Classroom, and any other apps the kids need their paws on.</p>
       </div>
     );
