eb42d9ed9b4af8ade90051dd431e169be4f311bc
Andrew Koop <koop@indiana.edu>
2018-05-30 08:07:24 -0400

    orange

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           Hi, there. Welcome.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
-        <h2>WORKworkWORKworkWORK</h2>
+        <h2>WORK<span class="orangish">work</span>WORK<span class="orangish">work</span>WORK</h2>
         <p>I studied to be a copywriter and wrote grants. I made <span class="ital">oh-so-many</span> websites.</p>
         <p>Today, I write data integration Powershell scripts for Microsoft, Canvas, Google, Apple Classroom, and any other apps the kids need their paws on.</p>
       </div>
