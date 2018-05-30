d40d7f9eafc154bd8b22a6671ce1937a060e1680
Andrew Koop <koop@indiana.edu>
2018-05-30 01:03:47 -0400

    sharbus

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ class App extends Component {
           <h1 className="App-title">Andrew Koop</h1>
         </header>
         <p className="App-intro">
-          <p>Welcome to my tiny site.</p> <p>I'm a database programmer and educator.</p> <p> I live in a green little place called Bloomington, IN.</p>
-        
-        <p>I studied to be a copywriter and then wrote grants. Then I made a great many websites. These days, I mostly write data integration Powershell scripts for Microsoft, Google, Apple Classroom, and any other apps the kids need their paws on.</p>
+          Welcome to my tiny site.</p> <p>I'm a database programmer and educator.</p> <p> I live in a green little place called Bloomington, IN.</p>
+        <h2>Who Me?</h2>
+        <p>I studied to be a copywriter and then wrote grants.</p>
+        <p> Then I made a great many websites. These days, I mostly write data integration Powershell scripts for Microsoft, Google, Apple Classroom, and any other apps the kids need their paws on.</p>
       </div>
     );
   }
