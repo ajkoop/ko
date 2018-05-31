6465e6ffd024ab94d9e419b9066f8b5d51abf738
Andrew Koop <koop@indiana.edu>
2018-05-31 17:18:21 -0400

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
           Hi, there. Welcome.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
         <h2>WORK<span className="orangish">work</span>WORK<span className="orangish">work</span>WORK</h2>
         <p>I studied to be a copywriter and wrote grants. I made <span className="ital">oh-so-many</span> websites.</p>
-        <p>Today, I write data migration scripts for educational and productivity apps.</p>
+        <p>Today, I write data migration scripts for educational (Canvas, Apple Classroom) and productivity apps such as (Google, Microsoft).</p>
       </div>
       </div>
     );
