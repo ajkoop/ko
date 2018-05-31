28fea7dcab4de0173a156e5b42176685006730e1
Andrew Koop <koop@indiana.edu>
2018-05-31 17:49:07 -0400

    gotta get that em dash just right

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
           Hi, there. Welcome.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
         <h2>WORK<span className="orangish">work</span>WORK<span className="orangish">work</span>WORK</h2>
         <p>I studied to be a copywriter and wrote grants. I made <span className="ital">oh-so-many</span> websites.</p>
-        <p>Today, I write data migration scripts for MCCSC -- mostly in PowerShell. I do a lot of rostering/account creation work to interface with the following companies.</p>
+        <p>Today, I write data migration scripts for MCCSC &#8212; mostly in PowerShell. I do a lot of rostering/account creation work to interface with the following companies.</p>
         <ul>
           <li>Canvas</li>
           <li>Google</li>
