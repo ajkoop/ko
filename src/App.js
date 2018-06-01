cedf50687a806d6b1fd9ebcde825ebb7c51eb907
Andrew Koop <koop@indiana.edu>
2018-06-01 06:42:28 -0400

    list

--- src/App.js
+++ src/App.js
@@ -15,12 +15,13 @@ class App extends Component {
           Hi, there. Welcome.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
         <h2>WORK<span className="orangish">work</span>WORK<span className="orangish">work</span>WORK</h2>
         <p>I studied to be a copywriter and wrote grants. I made <span className="ital">oh-so-many</span> websites.</p>
-        <p>Today, I write data migration scripts for MCCSC &#8212; mostly in PowerShell. I do a lot of account synchronization work to interface with education and productivity companies including:</p>
+        <p>Today, I write data migration scripts for MCCSC &#8212; mostly in PowerShell. I work on account synchronization for education and productivity companies. Here are a few:</p>
         <ul>
           <li>Canvas</li>
           <li>Google</li>
           <li>Microsoft</li>
           <li>Apple</li>
+          
         </ul>
       </div>
       </div>
