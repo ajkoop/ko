1888bc5a27fb553ccc5c108d954d96681ce2c211
Andrew Koop <koop@indiana.edu>
2018-06-21 11:25:26 -0400

    shango bax

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <div className="mainstreet">
         <p className="App-intro">
           Hi, there. Welcome.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
-        <h2>wOr<span class="obnox">K</span></h2>
+        <h2>wOr<span class="obnox">k</span></h2>
         <p>I studied to be a copywriter and wrote grant and made <span className="ital">oh-so-many</span> websites.</p>
         <p>Today, I write data migration scripts for MCCSC. I sync our user accounts for vendors such as </p> 
         <ul>
