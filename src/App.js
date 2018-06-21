8c044bddbd0414f78ae4d8c293db43b89dc118ec
Andrew Koop <koop@indiana.edu>
2018-06-21 09:58:40 -0400

    bango chango texty

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
         <p className="App-intro">
           Hi, there. Welcome.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
         <h2>wOrK</h2>
-        <p>I studied to be a copywriter and wrote grants. I made <span className="ital">oh-so-many</span> websites.</p>
-        <p>Today, I write data migration scripts for MCCSC. I sync user accounts for vendors such as </p> 
+        <p>I studied to be a copywriter and wrote grant and made <span className="ital">oh-so-many</span> websites.</p>
+        <p>Today, I write data migration scripts for MCCSC. I sync our user accounts for vendors such as </p> 
         <ul>
           <li>Canvas</li>
           <li>Google</li>
