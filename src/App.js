2c90cdf08de852d40dc5d7eb343298ebaf7021d3
Andrew Koop <koop@indiana.edu>
2019-03-17 10:57:26 -0400

    style change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <div className="mainstreet">
         <p className="App-intro">
           Hi, there. </p> <p>I'm a database programmer and Adjunct Lecturer with IU's Media School.</p>
-        <h2>MCCSC</h2>
+        <h2 class="mccsc">MCCSC</h2>
          <p>My daily programming efforts entail writing data migration scripts (primarily in Powershell and SQL) for the Monroe County Community School Corporation (MCCSC). I sync our user accounts for vendors such as </p> 
         <ul>
           <li>Canvas</li>
@@ -24,7 +24,7 @@ class App extends Component {
           <li>Apple</li>
           
         </ul>
-        <h2>Indiana University</h2>
+        <h2 class="iu">Indiana University</h2>
   <p>I have been teaching online portfolio devlopment within IU's Media School since 2013.</p>
         
       </div>
