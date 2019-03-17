242ef9f4b7439e17cd932bbc940b2436db69befa
Andrew Koop <koop@indiana.edu>
2019-03-17 09:45:02 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         </header>
         <div className="mainstreet">
         <p className="App-intro">
-          Hi, there. Welcome.</p> <p>I'm a database programmer and <a href="http://mediaschool.indiana.edu/profile/?p=koop">educator</a> in Bloomington, IN.</p>
+          Hi, there. Welcome.</p> <p>I'm a database programmer and Adjunct Lecturer with IU's Media School.</p>
         <h2>wOr<span class="obnox">k</span></h2>
          <p>Today, I write data migration scripts for MCCSC. I sync our user accounts for vendors such as </p> 
         <ul>
@@ -24,9 +24,9 @@ class App extends Component {
           <li>Apple</li>
           
         </ul>
-        <h2>gRowT<span class="obnox">h</span></h2>
+       <!-- <h2>gRowT<span class="obnox">h</span></h2>
 <p>I love to learn new languages. Check my <a href="https://app.pluralsight.com/profile/koop">latest stats</a> (if you are into stats)!</p>
-
+-->
         
       </div>
       </div>
