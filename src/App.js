5cc2629af6bfecabf9abb023675c796a5124bc86
Andrew Koop <aj@li3t.com>
2022-04-11 20:51:18 -0400

    bomp

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         <div className="mainstreet">
         <p className="App-intro">
           Hi, there. </p> <p>I'm a database programmer and educator.</p>
-        <h2 class="mccsc">MCCSC</h2>
+        <h2 class="mccsc">Assistant Director of Technology - MCCSC</h2>
          <p>My daily programming efforts entail writing data migration scripts (primarily in Powershell and SQL) for the Monroe County Community School Corporation (MCCSC). I onboard, modify, and offboard  user accounts for vendors such as  
         
           Canvas,
@@ -25,7 +25,7 @@ class App extends Component {
           Apple.</p>
           
         
-        <h2 class="iu">Indiana University</h2>
+        <h2 class="iu">Adjunct Lecturer - Indiana University</h2>
   <p>I have been teaching online portfolio development within IU's Media School since 2013.</p>
         
       </div>
